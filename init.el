d74a4c25d715dbcc0545c21edd9075e4c09035e0
Jose Flores <josecruzflores.fl@gmail.com>
2020-07-18 19:18:00 -0500

    Add melpa to config

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages") t)
+(package-initialize)
+
 (setq-default major-mode 'text-mode)
 
 (add-hook 'emacs-lisp-mode-hook 'electric-pair-mode)
-
-
