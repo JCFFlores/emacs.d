b2ec501ab8bc6c75e1284c36fefcf03d1bd78d8c
Jose Flores <josecruzflores.fl@gmail.com>
2020-07-18 22:18:18 -0500

    Add rainbow delimiters to programming modes

--- init.el
+++ init.el
@@ -2,9 +2,13 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(require 'rainbow-delimiters)
 (setq-default major-mode 'text-mode)
 
 (add-hook 'prog-mode-hook 'electric-pair-mode)
 
 ;; Enable line numbers for all files
 (global-display-line-numbers-mode 1)
+
+;; Add rainbow-delimiters
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
