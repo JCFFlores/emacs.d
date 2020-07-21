32507c63027c0e1f5843f68de6e7671ab748b1ce
Jose Flores <josecruzflores.fl@gmail.com>
2020-07-20 21:18:42 -0500

    Install all-the-icons and use it for dired

--- init.el
+++ init.el
@@ -17,6 +17,13 @@
   :ensure t
   :bind (("C-x g" . magit-status)))
 
+(use-package all-the-icons
+  :ensure t)
+
+(use-package all-the-icons-dired
+  :ensure t
+  :hook (dired-mode . all-the-icons-dired-mode))
+
 ;; Configure keyfreq
 (use-package keyfreq
   :ensure t
