4ba54677c6b0c9412f67685c1d929829d6d723a4
Jose Flores <josecruzflores.fl@gmail.com>
2020-07-21 00:10:20 -0500

    Upate custom.el

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
     ("d0aa1464d7e55d18ca1e0381627fac40229b9a24bca2a3c1db8446482ce8185e" "3682b8de9b3576ed44d3b8c96340bc39dc44116b5ffa178a6e7a834444e362de" default)))
  '(package-selected-packages
    (quote
-    (all-the-icons-dired all-the-icons tron-legacy-theme sorcery-theme toc-org keyfreq use-package counsel magit rainbow-delimiters)))
+    (org-plus-contrib fish-mode backup-each-save haskell-mode all-the-icons-dired all-the-icons tron-legacy-theme sorcery-theme toc-org keyfreq use-package counsel magit rainbow-delimiters)))
  '(pdf-view-midnight-colors (quote ("#dddddd" . "#000000"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
