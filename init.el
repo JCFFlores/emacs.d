a5cd752e64c2f6e7d98db5a56cf27c8ec9ce316c
Jose Flores <josecruzflores.fl@gmail.com>
2020-07-31 00:15:57 -0500

    Move org configuration to literate config

--- init.el
+++ init.el
@@ -1,12 +1,5 @@
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-(use-package org
-  :ensure org-plus-contrib
-  :bind (("C-c a" . org-agenda))
-  :mode (("\\.org\\'" . org-mode))
-  :config
-  (add-to-list 'org-modules 'org-habit))
-
 ;; Configure keyfreq
 (use-package keyfreq
   :ensure t
@@ -27,24 +20,6 @@
   :ensure t
   :hook (after-save . backup-each-save))
 (setq make-backup-files nil)
-
-;; Org configuration
-;; Set location for agenda files
-(setq org-agenda-files (list (getenv "ORG_AGENDA")))
-
-;; Keywords used in org todo items
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "IN-PROGRESS(i)" "|" "DONE(d)" "CANCELED(c)" "MISSED(m)")))
-
-;; Colors for todo states
-(setq org-todo-keyword-faces
-      '(("CANCELED" . "magenta") ("MISSED" . "red")))
-
-;; Max number of days to show in org agenda
-(setq org-agenda-span 14)
-
-;; Make org agenda not start on a fixed day
-(setq org-agenda-start-on-weekday nil)
 
 ;; Follow symlinks
 (setq vc-follow-symlinks t)
