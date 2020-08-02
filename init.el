a8387926045c71d167f64081cc65f14847d4f8e9
Jose Flores <josecruzflores.fl@gmail.com>
2020-08-02 18:27:24 -0500

    Move all configuration to literate config

--- init.el
+++ init.el
@@ -1,6 +1,2 @@
+;; The configuration resides in the file "README.org" as a literate config file
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
-
-;; Follow symlinks
-(setq vc-follow-symlinks t)
-
-(setq fill-column 80)
