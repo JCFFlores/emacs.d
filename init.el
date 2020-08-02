05543212b03a46a65373b737486ecf68475dd07b
Jose Flores <josecruzflores.fl@gmail.com>
2020-08-02 17:53:45 -0500

    Move backup configuration to literate config

--- init.el
+++ init.el
@@ -1,11 +1,5 @@
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-;; Configure emacs backups
-(use-package backup-each-save
-  :ensure t
-  :hook (after-save . backup-each-save))
-(setq make-backup-files nil)
-
 ;; Follow symlinks
 (setq vc-follow-symlinks t)
 
