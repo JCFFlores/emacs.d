5baf14e70f6c5cfbd3d1817dd7eccbd9ccc714f9
Jose Flores <josecruzflores.fl@gmail.com>
2020-08-02 10:11:41 -0500

    Move counsel configuration to literate config

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
-;; Counsel config
-(use-package counsel :ensure t)
-(ivy-mode 1)
-(counsel-mode 1)
-
 ;; Configure emacs backups
 (use-package backup-each-save
   :ensure t
