4cc6baa62741e18ab0be4eba6da2a0445b1d0b3c
Jose Flores <josecruzflores.fl@gmail.com>
2020-12-07 20:13:04 -0600

    Run checkdoc on init.el

--- init.el
+++ init.el
@@ -1,2 +1,10 @@
-;; The configuration resides in the file "README.org" as a literate config file
+;;; package --- Emacs configuration
+
+;;; Commentary:
+;;; The configuration resides in the file "README.org" as a literate config file
+;;; Code:
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
+
+(provide 'init)
+
+;;; init.el ends here
