ca35ed1703944c9df69bb34915fb2bf613547117
Jose Flores <josecruzflores.fl@gmail.com>
2020-12-25 14:56:05 -0600

    Increase garbage collector in early-init.el

--- early-init.el
+++ early-init.el
@@ -4,6 +4,7 @@
 ;; Leverage early-init.el to speed up startup.
 
 ;;; Code:
+(setq gc-cons-threshold 100000000)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
