1fc6b90337bfa6d0edf6b8eac1aa87d539b7f8b1
Jose Flores <josecruzflores.fl@gmail.com>
2020-12-25 14:58:16 -0600

    Comment early-init.el

--- early-init.el
+++ early-init.el
@@ -4,8 +4,10 @@
 ;; Leverage early-init.el to speed up startup.
 
 ;;; Code:
+;; Defer garbage collection
 (setq gc-cons-threshold 100000000)
 
+;; Remove unwanted UI items
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
