50299df68c4ce698556f7cf4a4288664e7121b36
Jose Flores <josecruzflores.fl@gmail.com>
2020-12-25 14:52:13 -0600

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,14 @@
+;;; early-init.el --- Early configuration -*- lexical-binding:t -*-
+
+;;; Commentary:
+;; Leverage early-init.el to speed up startup.
+
+;;; Code:
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(provide 'early-init)
+
+;;; early-init.el ends here
