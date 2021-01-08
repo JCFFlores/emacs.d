2490dc8273a172285064c0f8909c2d61a7f75458
Jose Flores <josecruzflores.fl@gmail.com>
2021-01-08 13:04:25 -0600

    Stop trying to byte-compile config

--- init.el
+++ init.el
@@ -3,12 +3,8 @@
 ;;; Commentary:
 ;;; The configuration resides in the file "README.org" as a literate config file
 ;;; Code:
-(let* ((file-name-handler-alist nil)
-       ;; Setting file-name-handler-alist to nil during initialization speeds up start up
-       (compiled-file (expand-file-name "README.elc" user-emacs-directory)))
-  (if (file-exists-p compiled-file)
-      (load-file compiled-file)
-    (org-babel-load-file (expand-file-name "README.org" user-emacs-directory)))
+(let* ((file-name-handler-alist nil)) ;; Setting file-name-handler-alist to nil during initialization speeds up start up
+  (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 
   (provide 'init))
 
